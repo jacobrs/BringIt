d24cac5938fcef7edc2db7f961270e6db731af33
jacobrs <jacob_gagne@hotmail.com>
2016-02-20 18:48:54 -0500

    Added the user model

--- config/server_config.js
+++ config/server_config.js
@@ -1,5 +1,6 @@
 var config = {};
 
 config.port = 80;
+config.firebase_url = "https://delivernow.firebaseio.com/";
 
 module.exports = config;
--- server.js
+++ server.js
@@ -3,9 +3,13 @@ global.appRoot = path.resolve(__dirname);
 
 var express = require('express');
 var exhbs = require('express-handlebars');
+var Firebase = require("firebase");
+var User = require(appRoot+"/app/models/user");
 
 var app = express();
 var config = require(appRoot+"/config/server_config");
+
+global.rootRef = new Firebase(config.firebase_url);
 
 app.engine('.hbs',  exhbs({
 	extname: '.hbs',
