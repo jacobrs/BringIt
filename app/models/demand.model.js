7bf9d6ecc382cff4927738e33f76e622be8eb897
benrs <benjaminbarault@gmail.com>
2016-02-21 07:30:41 -0500

    Pushing a bunch of changes

--- app/models/demand.model.js
+++ app/models/demand.model.js
@@ -3,6 +3,25 @@ var Demand = function (data) {
   this.place = function(){
     var postsRef = global.rootRef.child("demands");
     postsRef.push().set({
+      owner: this.data.owner,
+      fname: this.data.asker,
+      comments: this.data.comments,
+      deliverer: this.data.deliverer,
+      destination: {
+        label: this.data.destination.label,
+        long: this.data.destination.long,
+        lat: this.data.destination.lat
+      },
+      item: this.data.item,
+      price: this.data.price,
+      shop: this.data.shop,
+      tip: this.data.tip
+    });
+  };
+  this.placeWithID = function(id){
+    var postsRef = global.rootRef.child("demands/"+id);
+    postsRef.set({
+      owner: this.data.owner,
       fname: this.data.asker,
       comments: this.data.comments,
       deliverer: this.data.deliverer,
