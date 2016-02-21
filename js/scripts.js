deb459785a0dcd79a6cd72fc7115e8ba7eef9b1c
darrinfong <darrinfong@gmail.com>
2016-02-20 19:20:04 -0500

    fixed scripts in frontpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,7 @@
-
-$(document).ready(function(){/* google maps -----------------------------------------------------*/
-google.maps.event.addDomListener(window, 'load', initialize);
-
-function initialize() {
-
-  /* position Amsterdam */
-  var latlng = new google.maps.LatLng(52.3731, 4.8922);
-
-  var mapOptions = {
-    center: latlng,
-    scrollWheel: false,
-    zoom: 13
-  };
-  
-  var marker = new google.maps.Marker({
-    position: latlng,
-    url: '/',
-    animation: google.maps.Animation.DROP
+function initMap() {
+  var mapDiv = document.getElementById('map-canvas');
+  var map = new google.maps.Map(mapDiv, {
+    center: {lat: 44.540, lng: -78.546},
+    zoom: 8
   });
-  
-  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-  marker.setMap(map);
-
-};
-/* end google maps -----------------------------------------------------*/
-});
+}
