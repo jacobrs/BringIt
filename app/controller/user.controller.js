d1a9c4a1f3bdf9f38af89e51a9537fdb7e5830af
benrs <benjaminbarault@gmail.com>
2016-02-20 19:24:24 -0500

    Added function stubs

--- app/controller/user.controller.js
+++ app/controller/user.controller.js
@@ -1,17 +1,57 @@
-var User =  require(appRoot+"/app/models/user.model.js");
+var funcs = require(appRoot+"/config/global_functions.js"); 
 
 exports.getUser = function(req, res, next){
+	var body = req.body;
+	var resp = {};
 
+	if(!funcs.isUndef(body.username)){
+		funcs.getUser(body.username, req, res, next);				
+	}else{
+		resp.error = "Invalid use of api";
+		resp.code  = 400;
+		res.json(resp);
+		res.end();
+	}
 };
 
 exports.postUser = function(req, res, next){
+	var body = req.body;
+	var resp = {};
 
+	if(!funcs.isUndef(body.username) && !funcs.isUndef(body.email) && !funcs.isUndef(body.password)){
+		funcs.createUser(body, req, res, next);
+	}else{
+		resp.error = "Invalid use of api";
+		resp.code  = 400;
+		res.json(resp);
+		res.end();
+	}
 };
 
 exports.putUser = function(req, res, next){
+	var body = req.body;
+	var resp = {};	
 
+	if(!funcs.isUndef(body.username) || !funcs.isUndef(body.email) || !funcs.isUndef(body.password)){
+		funcs.updateUser(body, req, res, next);
+	}else{
+		resp.error = "Invalid use of api";
+		resp.code  = 400;
+		res.json(resp);
+		res.end();
+	}
 };
 
 exports.deleteUser = function(req, res, next){
+	var body = req.body;
+	var resp = {};
 
+	if(!funcs.isUndef(body.username)){
+		funcs.deleteUser(body.username, req, res, next);
+	}else{
+		resp.error = "Invalid use of api";
+		resp.code  = 400;
+		res.json(resp);
+		res.end();
+	}
 };
